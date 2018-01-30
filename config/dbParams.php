70340fe58368b002621dcc908ccc93bc8859163a
Sergey Lutsko <s.lutsko@gmail.com>
2018-01-30 10:06:06 +0600

    переименование БД

--- config/dbParams.php
+++ config/dbParams.php
@@ -1,7 +1,11 @@
 <?php
 return array(
     'host' => 'localhost',
-    'dbname' => 'albatros',
-    'user' => 'albatros',
+    'dbname' => 'q_albatros',
+    'user' => 'q_albatros',
     'password' => 'fdgkje903;5ej-rmf=g0%$9fsd0r-xc.'
     );
+
+// CREATE USER 'q_albatros'@'localhost' IDENTIFIED BY 'fdgkje903;5ej-rmf=g0%$9fsd0r-xc.'
+// GRANT ALL PRIVILEGES ON q_albatros.* TO 'q_albatros'@'localhost' WITH GRANT OPTION;
+
